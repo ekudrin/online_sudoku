b01529201ac2470f094670fbe0c4f027092bbd1b
kudrinev <chesterdelauren@gmail.com>
2024-06-21 12:46:06 +0300

    add find_empty,is_valid, solve

--- MainPage.py
+++ MainPage.py
@@ -10,7 +10,9 @@ from CellValues import CellValues
 def main():
     driver = setup_driver()
     driver.get("https://sudokuhit.com/ru/")
-    copy_board(driver)
+    board = copy_board(driver)
+    solve(board)
+    print(board)
 
 
 def setup_driver():
@@ -34,6 +36,7 @@ def copy_board(driver):
         elem_row, elem_col, elem_value = get_indexes_and_values(element)
         board[elem_row][elem_col] = elem_value
     print(board)
+    return board
 
 
 def get_indexes_and_values(element):
@@ -61,12 +64,59 @@ def get_indexes_and_values(element):
     return int(row), int(column), int(value)
 
 
-# start
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def is_valid(board, num, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
 
 
 main()
-# copy board
 
 # solve
+
 
 # fill on site
